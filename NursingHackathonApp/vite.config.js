ab2be17e83f62eff75279f0b3cf522fb80a8bd1f
Nicholas Lor <bobbylor00@gmail.com>
2023-03-04 12:05:26 -0700

    vite config v3 bruh moment

--- NursingHackathonApp/vite.config.js
+++ NursingHackathonApp/vite.config.js
@@ -3,4 +3,15 @@ import { fileURLToPath } from 'url';
 
 export default defineConfig({
     base:'/NursingHackathonApp2/',
+
+    build: {
+    rollupOptions: {
+        outDir: 'dist',
+        input: {
+          index: fileURLToPath(new URL('index.html', import.meta.url)),
+          menu: fileURLToPath(new URL('menu.html', import.meta.url)),
+          Postop: fileURLToPath(new URL('Post-op.html', import.meta.url)),
+        },
+      },
+}
 })
