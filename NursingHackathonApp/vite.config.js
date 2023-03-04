a638e9e84b9ac3ebd8cbc2bfa422210c2b9d8ad9
Shawn Lee <73615355+chansuck@users.noreply.github.com>
2023-03-04 13:47:49 -0700

    Update vite.config.js

--- NursingHackathonApp/vite.config.js
+++ NursingHackathonApp/vite.config.js
@@ -13,7 +13,9 @@ export default defineConfig({
           PostOp: fileURLToPath(new URL('Post-Op.html', import.meta.url)),
           myAccount: fileURLToPath(new URL('myAccount.html', import.meta.url)),
           surgicalProcedure: fileURLToPath(new URL('surgicalProcedure.html', import.meta.url)),
-          beforeAndAfterHipArthroplasty: fileURLToPath(new URL('beforeAndAfterHipArthroplasty.html', import.meta.url)),
+          beforeAndAfterHipArthroplasty: fileURLToPath(new URL('BeforeAfter.html', import.meta.url)),
+          dailyGoals: fileURLToPath(new URL('dailyGoals.html', import.meta.url)),
+          
         },
       },
     }
